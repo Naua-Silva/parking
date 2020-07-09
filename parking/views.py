d94c465b445c4b52b0c4fa7165f7ac9b23c35996
José Nauã da Silva <nauasholanda@gmail.com>
2020-07-09 11:04:04 -0300

    Method 'goingIn' created

--- parking/views.py
+++ parking/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 def index(request):
 
 	return render(request, 'parking/index.html')
+
+def goingIn(request):
+
+	if request.method == 'POST':
+		placa = request.POST.get('plate')
+
+		if placa is not "":
+			teste1 = Reserve.objects.filter(plate=placa, In=True)
+			if teste1:
+				teste2 = Reserve.objects.filter(plate=placa, In=True, paid=False)
+				teste3 = Reserve.objects.filter(plate=placa, In=True, paid=True)
+				if teste2:
+					messages.error(request, 'Veículo já dentro, para sair pague a reserva {}'.format(teste2.id))
+				elif teste3:
+					messages.error(request, 'Veículo já dentro com saída liberada')
+			else:
+				reserva = Reserve.objects.create(plate=placa)
+				messages.error(request, 'Número da reserva: {}'.format(reserva.id))
+
+		else:
+			messages.error(request, 'Digite uma placa válida')
+
+
+	return render(request, 'parking/entrada.html')
